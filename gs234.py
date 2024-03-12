4d76b2cd4e96bfecb488275af38187fe3b19d2a9
Kobus Goosen <kgoosen@videologyinc.com>
2024-03-12 15:16:25 +0100

    make python module to hold scripts

---
+++ gs234.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import sys
+from gs234py import flashapp, flashisp, readreg, status, sync_trigger, writereg
+
+
+def usage():
+    print(f"Usage: {__name__} command [args] [--help]")
+    print(f"Commands:")
+    print(f"  readreg")
+    print(f"  flashisp")
+    print(f"  flashapp")
+    print(f"  status")
+    print(f"  sync_trigger")
+    print(f"  writereg")
+
+if __name__ == "__main__":
+    # get the first argument
+    first_arg = sys.argv[1]
+    if first_arg in ["readreg", "flashisp", "flashapp", "status", "sync_trigger", "writereg"]:
+        # remove the first argument
+        sys.argv.pop(1)
+        if first_arg == "readreg":
+            readreg.main()
+        elif first_arg == "flashisp":
+            flashisp.main()
+        elif first_arg == "flashapp":
+            flashapp.main()
+        elif first_arg == "status":
+            status.main()
+        elif first_arg == "sync_trigger":
+            sync_trigger.main()
+        elif first_arg == "writereg":
+            writereg.main()
+    else:
+        usage()
+        sys.exit(1)
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='gs234py',
+    version='0.1.0',
+    packages=['gs234py'],
+    package_dir={'gs234py': 'gs234py'},
+    scripts=['gs234.py']
+)
