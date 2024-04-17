1309f84a5dc7efbd0ee3ce1df328882ca26ffec1
Kobus Goosen <KobusG@users.noreply.github.com>
2024-04-17 10:43:31 +0200

    add reboot and flashnvm commands

--- ap1302py/__main__.py
+++ ap1302py/__main__.py
@@ -1,16 +1,15 @@
-#!/usr/bin/env python3
-# SPDX-License-Identifier: GPL-2.0-or-later
-
 import sys
-from . import flashapp, flashisp, readreg, status, sync_trigger, writereg
+from . import reboot, flashapp, flashisp, flashnvm, readreg, status, sync_trigger, writereg
 
 
 def usage():
     print(f"Usage: {__name__} command [args] [--help]")
     print(f"Commands:")
     print(f"  readreg")
+    print(f"  reboot")
     print(f"  flashisp")
     print(f"  flashapp")
+    print(f"  flashnvm")
     print(f"  status")
     print(f"  sync_trigger")
     print(f"  writereg")
@@ -21,15 +20,19 @@ def main():
         usage()
         sys.exit(1)
     first_arg = sys.argv[1]
-    if first_arg in ["readreg", "flashisp", "flashapp", "status", "sync_trigger", "writereg"]:
+    if first_arg in ["readreg", "reboot", "flashisp", "flashapp", "flashnvm", "status", "sync_trigger", "writereg"]:
         # remove the first argument
         sys.argv.pop(1)
         if first_arg == "readreg":
             readreg.main()
+        elif first_arg == "reboot":
+            reboot.main()
         elif first_arg == "flashisp":
             flashisp.main()
         elif first_arg == "flashapp":
             flashapp.main()
+        elif first_arg == "flashnvm":
+            flashnvm.main()
         elif first_arg == "status":
             status.main()
         elif first_arg == "sync_trigger":
