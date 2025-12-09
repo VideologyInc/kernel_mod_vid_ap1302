ed86291b9a99e7f3ec3e0713ff28498c903286db
Aleksandr Chubuchnyi <chubuchnyi@gmail.com>
2025-12-09 16:08:52 +0400

    Improve restore registers logic to handle NVM recall and reapply white balance and color effects

--- cam_ar0234.c
+++ cam_ar0234.c
@@ -526,8 +526,12 @@ static int gs_ar0234_s_ctrl(struct v4l2_ctrl *ctrl)
 		dev_dbg_ratelimited(sd->dev, "%s: set store registers\n", __func__);
 		break;
 	case V4L2_CID_RESTORE_REGISTERS:
-		ret = gs_ar0234_write_reg8(sensor, GS_REG_SAVE_RESTART, 0x99);
-		dev_dbg_ratelimited(sd->dev, "%s: restore from NVM via reboot\n", __func__);
+		ret = gs_ar0234_write_reg8(sensor, GS_REG_SAVE_RESTART, 0x05);
+		if (!ret) {
+        ret = gs_ar0234_i_cntrl(sensor);
+        if (!ret)
+            gs_ar0234_reapply_wb_and_colorfx(sensor);
+    	}
 		break;
 	case V4L2_CID_RESTORE_FACTORY:
 		ret = gs_ar0234_write_reg8(sensor, GS_REG_SAVE_RESTART, 0x07);
